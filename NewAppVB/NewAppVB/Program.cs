cfbef5e407b32f2903917cfde74890a74d1d740b
Shiloh Mount <shilohjonze@gmail.com>
2017-02-08 21:49:38 -0500

    begining of madlibs app

--- NewAppVB/NewAppVB/Program.cs
+++ NewAppVB/NewAppVB/Program.cs
@@ -4,4 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-"
+"<h2>There once was a [adjective] porgrammer who wanted to use JavaScript to [verb] the [noun].</h2>"
+
+var adjective = prompt('Please type an adjective');
+var sentence = "<h2>There once was a " + adjective;
+var verb = prompt('Please type a verb');
+var noun = prompt('Please type a noun');
+
+document.write(sentence);
